789487cdbf4b863f9bb459e2d0ba1ca24b6aff2b
yanis <yanis@PC-YANIS.local>
2021-01-18 18:57:45 +0100

    div mal fermée

--- Forum/view/header.php
+++ Forum/view/header.php
@@ -14,7 +14,7 @@
                         <h1>NOM DU FORUM </h1>
                 </div>
                 <div class="droite">
-                    <div class="bouton"
+                    <div class="bouton">
                         <a href="view/pageconnex.php"/>
                             <input id="b1" type="submit" value="Connexion"/>
                             <input id="b2" type="submit" value="Inscription"/>
