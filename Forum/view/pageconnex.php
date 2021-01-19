3617610f9305e81728e597394cc7799fb16e8edd
Corentin <corentinrubio63@gmail.com>
2021-01-19 08:52:27 +0100

    mis a jour du fichier pageconnex.css

--- Forum/view/pageconnex.php
+++ Forum/view/pageconnex.php
@@ -24,7 +24,12 @@
 	            </div>
                 <div class="btn_envoi">
                     <input type="submit" value="Se connecter">
-                    <p> Les cookies nous aident à fournir nos services. En utilisant nos Services ou en cliquant sur J'accepte, vous acceptez notre utilisation des cookies. Apprendre encore plus </p>
+                    <div class="bas">
+                    	<input type="checkbox">
+                    	<div class="text_bas">
+                    		<p> Les cookies nous aident à fournir nos services. En utilisant nos Services ou en cliquant sur J'accepte, vous acceptez notre utilisation des cookies. Apprendre encore plus </p>
+                    	</div>
+                    </div>
                 </div>
             </form>
         </div>
@@ -34,59 +39,65 @@
             <div class="titreconnex">
                 <h1>Création d'un compte.</h1>
             </div>
-            <form action="../after_inscri.php" method="post">
-                <div class="saisies">
-                    <div class="nom_saisies">
-                        <p>Votre prénom :</p>
-                    </div>
-                    <div class="sais">
-                        <input type="text" name="prenom" required/>
-                    </div>
-                </div>
-                <div class="saisies">
-                    <div class="nom_saisies">
-                        <p>Votre nom :</p>
-                    </div>
-                    <div class="sais">
-                       <input type="text" name="nom" required/>
-                    </div>
-                </div>
-                <div class="saisies">
-                    <div class="nom_saisies">
-                        <p>Votre email :</p>
-                    </div>
-                    <div class="sais">
-                        <input type="text" name="email" required/>
-                    </div>
-                </div>
-                <div class="saisies">
-                    <div class="nom_saisies">
-                        <p>Votre mot de passe :</p>
-                        <p>Confirmez votre mot de passe :</p>
-                    </div>
-                    <div class="sais">
-                        <input type="password" name="mdp" required/>
-                        <input type="password" name="mdp_conf" required/>
-                    </div>
-                </div>
-                <div class="saisies">
-                    <div class="nom_saisies">
-                        <p>Votre âge :</p>
-                    </div>
-                    <div class="sais">
-                        <input type="integer" name="âge" required/>
-                    </div>
-                </div>
-                <div class="saisies">
-                    <div class="nom_saisies">
-                        <p>Votre pseudo :</p>
-                    </div>
-                    <div class="sais">
-                        <input type="text" name="Pseudo" required/>
-                    </div>
-                </div>
+            <form class="form_connex2" action="../after_inscri.php" method="post">
+            	<div class="all_saisie">
+	                <div class="saisies">
+	                    <div class="nom_saisies">
+	                        <p>Votre prénom :</p>
+	                    </div>
+	                    <div class="sais">
+	                        <input type="text" name="prenom" required/>
+	                    </div>
+	                </div>
+	                <div class="saisies">
+	                    <div class="nom_saisies">
+	                        <p>Votre nom :</p>
+	                    </div>
+	                    <div class="sais">
+	                       <input type="text" name="nom" required/>
+	                    </div>
+	                </div>
+	                <div class="saisies">
+	                    <div class="nom_saisies">
+	                        <p>Votre email :</p>
+	                    </div>
+	                    <div class="sais">
+	                        <input type="text" name="email" required/>
+	                    </div>
+	                </div>
+	                <div class="saisies">
+	                    <div class="nom_saisies">
+	                        <p>Votre mot de passe :</p>
+	                    </div>
+	                    <div class="sais">
+	                        <input type="password" name="mdp" required/>
+	                        <input type="password" placeholder="Confirmer votre mot de passe"name="mdp_conf" required/>
+	                    </div>
+	                </div>
+	                <div class="saisies">
+	                    <div class="nom_saisies">
+	                        <p>Votre âge :</p>
+	                    </div>
+	                    <div class="sais">
+	                        <input type="integer" name="âge" required/>
+	                    </div>
+	                </div>
+	                <div class="saisies">
+	                    <div class="nom_saisies">
+	                        <p>Votre pseudo :</p>
+	                    </div>
+	                    <div class="sais">
+	                        <input type="text" name="Pseudo" required/>
+	                    </div>
+	                </div>
+	            </div>
                 <div class="btn_envoi">
-                    <input type="submit" value="S'incrire">
+	                <input type="submit" value="S'incrire">
+                    <div class="bas">
+	                    <div class="text_bas">
+	                    	<p> *Champs obligatoires </p>
+	                    </div>
+	                </div>
                 </div>
             </form>
         </div>
