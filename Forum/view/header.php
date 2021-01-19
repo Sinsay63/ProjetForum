285845f1545d55e5a5df4c0ecec5772044eb6059
Nathim <nathimrichar@gmail.com>
2021-01-19 11:56:01 +0100

    r

--- Forum/view/header.php
+++ Forum/view/header.php
@@ -13,12 +13,19 @@
                 <div class="Titre">
                         <h1>NOM DU FORUM </h1>
                 </div>
+                <label for="site-search">Search the site:</label>
+                <input type="search" id="site-search" name="q"
+                aria-label="Search through site content">
+                <button>Search</button>
                 <div class="droite">
                     <div class="bouton">
                         <a href="view/pageconnex.php"/>
                             <input id="b1" type="submit" value="Connexion"/>
                             <input id="b2" type="submit" value="Inscription"/>
                         </a>
+                    </div>
+                    <div class="modenuit">
+                        <button id="darkTrigger">Activer le thème sombre</button>
                     </div>
                 </div>
             </div>
