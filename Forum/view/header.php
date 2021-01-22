a4ae7d9d8575997ebd014fe9c8350eea74c9143e
Nathim <nathimrichar@gmail.com>
2021-01-22 14:44:52 +0100

    nath

--- Forum/view/header.php
+++ Forum/view/header.php
@@ -18,12 +18,22 @@
                 aria-label="Search through site content">
                 <button>Search</button>
                 <div class="droite">
+                    <?php
+                    if (isset($_SESSION['pseudo'])){
+                        echo 'Bonjour '.$_SESSION['pseudo'];
+                        echo '<a href="logout.php"><input type="button" value="deconnexion"></a>';
+                    }
+                    else{
+                    ?>
                     <div class="bouton">
                         <a href="view/pageconnex.php"/>
                             <input id="b1" type="submit" value="Connexion"/>
                             <input id="b2" type="submit" value="Inscription"/>
                         </a>
                     </div>
+                    <?php
+                    }
+                    ?>
                     <div class="modenuit">
                         <button id="darkTrigger">Activer le thème sombre</button>
                     </div>
