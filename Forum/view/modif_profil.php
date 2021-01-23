17b4bd19443ded209054b1183d6a41347edfdfeb
Corentin <corentinrubio63@gmail.com>
2021-01-23 20:20:10 +0100

    creation de la page modif_profil.php

---
+++ Forum/view/modif_profil.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="view/css/post.css"/>
+    </head>
+    <body>
+    	<div>
+    		<button class="modif_avatar">changer mon avatar</button>
+    	</div>
+    	<div>
+    		<p> mot de passe : </p>
+    		<input type="password" name="password" placeholder="Ecrivez un nouveau mot de passe" required/>
+    		<input type="password" name="password" placeholder="Confirmez mot de passe" required/>
+    	</div>
+    	<div>
+    		<p> E-mail : </p>
+    		<input type="text" name="mail" placeholder="Ecrivez une nouvelle adresse mail" required/>
+    	</div>
+    	<div>
+    		<input type="submit" value="valider les changements">
+    	</div>
+    </body>
+</html>
