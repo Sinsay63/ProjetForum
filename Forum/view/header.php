6e84a642390d025941a13a75b576d88414495269
Nathim <nathimrichar@gmail.com>
2021-02-05 16:52:24 +0100

    headercss

--- Forum/view/header.php
+++ Forum/view/header.php
@@ -9,26 +9,36 @@
             <div class="top">
                 <div class="gauche">
                     <a href="index.php"/><img class="logo" src="view/images/logo.png" alt=""/></a>
-                
-                <div class="Titre">
-                    <h1>NOM DU FORUM </h1>
                 </div>
-                <label for="site-search" id="txt_search">Search the site:</label>
-                <input type="search" id="site-search" name="q" aria-label="Search through site content">
-                <button id="search">Search</button>
+                <div class="mid">
+                    <div class="Titre">
+                        <h1 class="title">AnecdoteSpecialChars </h1>
+                    </div>
+                    <div class="recherche">
+                        <label for="site-search" id="txt_search">Search the site:</label>
+                        <input type="search" id="site-search" name="q" aria-label="Search through site content">
+                        <button id="search">Search</button>
+                    </div>  
                 </div>
                 <div class="droite">
                     <?php
                     if (isset($_SESSION['pseudo'])){
-                        echo 'Bonjour '.$_SESSION['pseudo'];?><img class="pp" src="<?php echo $_SESSION['Avatar'];   ?>"/>      <?php
-                        echo '<a href="index.php?page=deconnexion"><input type="button" value="deconnexion"></a>';
-                        echo '<a href="index.php?page=profil"><input type="button" value="Profil"></a>';
-                        
-                        if($_SESSION['IsAdmin']==1){
-                            echo '<a href="index.php?page=list_user"><input type="button" value="Users"></a>';
-                        }
+                        echo 'Bonjour '.$_SESSION['pseudo'];?>
+                        <div class="dropdown">
+                            <button type="button"> <img class="pp" src="<?php echo $_SESSION['Avatar'];   ?>"/></button> 
+                            <div class="content">
+                                <a href="index.php?page=profil"  value="Profil" >Profil</a>
+                                <a href="index.php?page=deconnexion"  value="deconnexion">Deconnexion</a>
+                                <?php
+                                if($_SESSION['IsAdmin']==1){
+                                    echo '<a href="index.php?page=list_user" value="Users"> Users </a>';
+                                }
+                                ?>
+                            </div>
+                        </div>
+                    <?php
                     }
-                    else{
+                        else{
                     ?>
                     <div class="bouton">
                         <a href="index.php?page=page_connexion"/>
