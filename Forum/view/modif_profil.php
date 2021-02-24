f6db7d4b84f6e3d7a9e72905d811a064aa9947d5
Corentin <corentinrubio63@gmail.com>
2021-02-24 10:50:23 +0100

    finish css de la page modif_profil

--- Forum/view/modif_profil.php
+++ Forum/view/modif_profil.php
@@ -17,78 +17,83 @@
                             <div class="img_avatar">
                                 <img src="<?php echo $value['Avatar_Path'];?>" class="pp_avatar">
                             </div>
-                            <div class="ava_droite">
-                                <?php 
-                                if(isset($err)){
-                                    if ($err==5){
-                                            $message="Le format de l'image est incorrect."; ?>
-                                            <p class='errorMessage2'><?php echo $message; ?></p>
-                                <?php   }
-                                        else if($err==6){
-                                            $message="La taille de l'image est trop grande."; ?>
-                                            <p class='errorMessage2'><?php echo $message; ?></p>
-                                <?php   }
-                                 else if($err==7){
-                                            $message="Une erreur lors de l'upload est survenue."; ?>
-                                            <p class='errorMessage2'><?php echo $message; ?></p>
-                                <?php   }
-                                }
-                                ?>
-                                <div class="choix_avatars">
-                                    <form action="index.php?page=new_avatar" method="post" enctype="multipart/form-data">
-                                        <div class="lien_modif_avatar">
-                                            <p> Modifier mon avatar </p>
-                                            <input type="file" name="avatar" id="fileUpload">
-                                            <input type="submit" name="submit" value="Upload">
-                                        </div>               
-                                    </form>
-                                </div>
-                            </div>
+                            <?php 
+                            if(isset($err)){
+                                if ($err==5){
+                                        $message="Le format de l'image est incorrect."; ?>
+                                        <div class="errorMessage2">
+                                            <p><?php echo $message; ?></p>
+                                        </div>
+                            <?php   }
+                                    else if($err==6){
+                                        $message="La taille de l'image est trop grande."; ?>
+                                        <div class="errorMessage2">
+                                            <p><?php echo $message; ?></p>
+                                        </div>
+                            <?php   }
+                             else if($err==7){
+                                        $message="Une erreur lors de l'upload est survenue."; ?>
+                                        <div class="errorMessage2">
+                                            <p><?php echo $message; ?></p>
+                                        </div>
+                            <?php   }
+                            }
+                            ?>
+                            <form class="choix_avatars" action="index.php?page=new_avatar" method="post" enctype="multipart/form-data">
+                                <input type="file" name="avatar" id="fileUpload" hidden>
+                                <label class="choix_fichier_avatar" for="fileUpload">Choisir un fichier</label>
+                                <input type="submit" name="submit" id="upload" value="Upload" hidden/>
+                                <label class="validation_avatar" for="upload">Ok</label>   
+                            </form>
                         </div>
                         <form class="modif_mdp" action="index.php?page=modif_profil" method="post">
                             <div class="mdp_container">
-                                <p> Changer votre mot de passe : </p>
+                                <p class="text_modif_mdp"> Changer votre mot de passe : </p>
                                  <?php 
                                     if((isset($err))){
                                         if ($err==1){
                                             $message="Le mot de passe est incorrect."; ?>
-                                            <p class='errorMessage2'><?php echo $message; ?></p>
+                                            <div class="errorMessage2">
+                                                <p><?php echo $message; ?></p>
+                                            </div>
                                 <?php   }
                                         else if($err==2){
                                             $message="Les mots de passe sont différents."; ?>
-                                            <p class='errorMessage2'><?php echo $message; ?></p>
+                                            <div class="errorMessage2">
+                                                <p><?php echo $message; ?></p>
+                                            </div>
                                 <?php   }
                                 else if($err==3){
                                             $message="L'ancien mot de passe et le nouveau correspondent."; ?>
-                                            <p class='errorMessage2'><?php echo $message; ?></p>
+                                            <div class="errorMessage2">
+                                                <p><?php echo $message; ?></p>
+                                            </div>
                                 <?php   }
                                     }   ?>
                                 <div class="mdp_box_container">
-                                    <input class="mdp_box" type="password" name="old_password" placeholder="Ancien mot de passe" minlength="5" required/>
-                                    <input class="mdp_box" type="password" name="new_password" placeholder="Nouveau mot de passe" minlength="5" required/>
-                                    <input class="mdp_box" type="password" name="new_password2" placeholder="Confirmez mot de passe" minlength="5" required/>
+                                    <input class="mdp_box" type="password" name="old_password" placeholder="Ancien mot de passe" minlength="3" required/>
+                                    <input class="mdp_box" type="password" name="new_password" placeholder="Nouveau mot de passe" minlength="3" required/>
+                                    <input class="mdp_box" type="password" name="new_password2" placeholder="Confirmez mot de passe" minlength="3" required/>
                                 </div>
-                                <div style="padding-top:20px;">
-                                    <input type="submit" value="Changer">
-                                </div>
+                            </div>
+                            <div class="validation">
+                                <input class="btn_validation" type="submit" value="Changer">
                             </div>
                         </form>
                         </div>
                         <div class="right">
                             <form class="pseudo" action="index.php?page=modif_profil" method="post">
-                                <div class="pseudo">
-                                    <p> Votre pseudo: <?php echo $value['Pseudo']; ?> </p>
-                                    <div>
-                                        <div><input type="text" name="new_username" placeholder="Nouveau pseudo" minlength="4" maxlength="15" required/></div>
-                                    </div>
-                                    <div>
-                                        <input type="submit" value="Changer">
-                                    </div>
+                                <div class="choix_pseudo">
+                                    <input class="bouton_modif_pseudo" type="submit" value="Ok">
+                                    <input class="box_modif_pseudo" type="text" name="new_username" placeholder="Nouveau pseudo" minlength="4" maxlength="15" required/>
                                 </div>
+                                <div class="votre_pseudo">
+                                    <p><?php echo $value['Pseudo']; ?> </p>
+                                </div> 
                             </form>
                             <form class="mail" action="index.php?page=modif_profil" method="post">
                                 <div class="mail_container">
-                                    <p> Votre e-mail :  <?php echo $value['Email']; ?> </p>
+                                    <p class="text_modif_mail"> Votre e-mail :  <?php echo $value['Email']; ?> </p>
                                     <?php 
                                     if((isset($err))){
                                         if ($err==4){
@@ -97,8 +102,8 @@
                                 <?php   }
                                     }  ?>
                                     <input class="mail_box" type="text" name="new_email" placeholder="Nouvelle adresse email" required/>
-                                    <div style="padding-top:20px;">
-                                        <input type="submit" value="Changer">
+                                    <div class="validation">
+                                        <input class="btn_validation_mail" type="submit" value="Changer">
                                     </div>
                                 </div>
                             </form>
