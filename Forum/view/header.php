c40ce4279e2d8a0360e48a287add9ab863917265
Sinsay63 <yanis.houdier@gmail.com>
2021-02-26 17:41:53 +0100

    dqsdqsd

--- Forum/view/header.php
+++ Forum/view/header.php
@@ -78,6 +78,7 @@
                     <?php
                     }
                     ?>
+                    </div>
                     <div class="nuit">
                         <input type="checkbox" class="checkbox" id="checkbox">
                         <label for="checkbox" class="label">
@@ -88,7 +89,6 @@
                     </div>
                 </div>
             </div>
-        </div>
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 319"><path  d="M0,64L48,64C96,64,192,64,288,80C384,96,480,128,576,122.7C672,117,768,75,864,69.3C960,64,1056,96,1152,96C1248,96,1344,64,1392,48L1440,32L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path></svg>
             <script type="text/javascript" src="view/javascript/modenuit.js"></script>
         </header>
