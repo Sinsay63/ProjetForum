501d3cb61a673417fbf09aac423efedaeb759a8b
Corentin <corentinrubio63@gmail.com>
2021-02-28 16:07:07 +0100

    ajout d'image dark theme

--- Forum/view/post.php
+++ Forum/view/post.php
@@ -7,18 +7,18 @@
         <script type="text/javascript" src="view/javascript/trix.js"></script>
     </head>
     <body>
-        <form class="container" action="index.php?page=creation_article" method="post">
+        <form class="huge_container" action="index.php?page=creation_article" method="post">
             <div class="main">
                 <div class="big_box">
                     <p> Création d'un sujet de discussion </p>
-                    <select name="id_categorie">
+                    <select class="choix_cat" name="id_categorie">
                         <option value="">Choisir une categorie</option>
                         <option value="1">+18</option>
                         <option value="2">vacances</option>
                         <option value="3">insolites</option>
                         <option value="4">genantes</option>
                     </select>
-                    <input type="text" name="titre" placeholder="Entrez un titre" maxlength="40" minlength="20">
+                    <input class="choix_titre" type="text" name="titre" placeholder="Entrez un titre" maxlength="40" minlength="20">
                     <input id="x" class="contenu" type="hidden" name="contenu" placeholder="Entrez votre message" maxlength="200" minlength="10">
                     <trix-editor class="editor" input="x"></trix-editor>
                     <input type="submit" value="Poster le topic">
