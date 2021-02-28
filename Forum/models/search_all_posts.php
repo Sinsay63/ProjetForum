2e3f99787ba08d13388a8ef13ad4bcfb35301c6b
Sinsay63 <yanis.houdier@gmail.com>
2021-02-28 20:44:56 +0100

    choix cate accueil

--- Forum/models/search_all_posts.php
+++ Forum/models/search_all_posts.php
@@ -13,8 +13,22 @@ if(isset($_GET['ID_auteur']) && isset($_GET['ID'])){
         $repa->execute(array($id_art));
         $com= $repa->fetchAll();
 }
+else if(isset($_GET['cat'])){
+    $cat=$_GET['cat'];
+    if($cat=='moi'){
+        $reponse = $bdd->prepare('select log.Pseudo,art.Titre, art.Contenu,art.ID, art.Date_Publication,art.ID_auteur,log.Avatar_Path,art.IsClosed,cat.images,cat.nom from logins as log,articles as art,categories as cat,article_categorie as art_cat where log.ID = ?and art.ID_auteur = ?and  art_cat.id_article = art.ID  and cat.id = art_cat.id_categorie');
+        $resultes=$reponse->execute(array($_SESSION['ID'],$_SESSION['ID']));
+        $resultes = $reponse->fetchAll();
+    }
+    else{
+        $reponse = $bdd->prepare('select log.Pseudo,art.Titre, art.Contenu,art.ID, art.Date_Publication,art.ID_auteur,log.Avatar_Path,art.IsClosed,cat.images,cat.nom from logins as log inner join articles as art on art.ID_auteur = log.ID inner join article_categorie as art_cat on art_cat.id_article = art.ID inner join categories as cat on cat.id = art_cat.id_categorie and cat.id= ?');
+        $resultes=$reponse->execute(array($cat));
+        $resultes = $reponse->fetchAll();
+    }
+}
 else {
-        $reponse = $bdd->query('select log.Pseudo,art.Titre, art.Contenu,art.ID, art.Date_Publication,art.ID_auteur,log.Avatar_Path,art.IsClosed,cat.images,cat.nom from logins as log inner join articles as art on art.ID_auteur = log.ID inner join article_categorie as art_cat on art_cat.id_article = art.ID inner join categories as cat on cat.id = art_cat.id_categorie');
+        $reponse = $bdd->prepare('select log.Pseudo,art.Titre, art.Contenu,art.ID, art.Date_Publication,art.ID_auteur,log.Avatar_Path,art.IsClosed,cat.images,cat.nom from logins as log inner join articles as art on art.ID_auteur = log.ID inner join article_categorie as art_cat on art_cat.id_article = art.ID inner join categories as cat on cat.id = art_cat.id_categorie');
+        $resultes=$reponse->execute(array());
         $resultes = $reponse->fetchAll();
 }
 
--- Forum/view/list_posts.php
+++ Forum/view/list_posts.php
@@ -4,99 +4,126 @@
         <meta charset="UTF-8">
         <title></title>
         <link rel="stylesheet" href="view/css/list_post.css"/>
+        <script>
+function redirige (myvalue) {
+    switch(myvalue){
+        case '1':
+            window.location.assign("index.php?page=accueil&cat=1");
+            break;
+        case '2':
+            window.location.assign("index.php?page=accueil&cat=2");
+            break;
+        case '3':
+            window.location.assign("index.php?page=accueil&cat=3");
+            break;
+        case '4':
+            window.location.assign("index.php?page=accueil&cat=4");
+            break;
+        case '5':
+            window.location.assign("index.php?page=accueil&cat=moi");
+            break;
+        default:
+            window.location.assign("index.php?page=page_connexion");
+            break;
+        }
+    }
+</script>
     </head>
     <body>  
         <div class="container">
             <div class="main">
                 <div class="top_cat">
                     <div class="titre_cat">
-                        <select class="cat_menu" name="cat_menu">
+                        <select class="cat_menu" name="cat_menu" onChange="redirige(this.value)">
                             <option value="" hidden>Recherche de catégories</option>
                             <option value="1">+18</option>
                             <option value="2">Vacances</option>
-                            <option value="2">Insolites</option>
-                            <option value="2">Génantes</option>
+                            <option value="3">Insolites</option>
+                            <option value="4">Génantes</option>
                         </select>
                     </div>
                     <?php
-            if (isset($_SESSION['pseudo'])){
-                ?>
-                    <div class="mes_topics">
-                        <select class="mes_topics_menu" name="mes_topics">
-                            <option value="" hidden>Mes topics</option>
-                            <option value="1">Propres créations</option>
-                            <option value="2">Topics aimés</option>
-                        </select>
-                    </div>
-                <div class="crea_sujet">
-                    <a href="index.php?page=crea_article"><input class="button_crea_sujet" type="button" value="Créer un nouveau sujet"></a>
-                </div> <?php
-            }
-        ?>   
-            </div>
-        <?php 
-            foreach ($resultes as $result) { ?>
-
-            <div class="article">
-                <div class="contenu">
-                    <div class="contenu_gauche">
-                        <div class="box_cat_img">
-                            <img class="img_cat" src="<?php echo $result['images']; ?>">
+                    if (isset($_SESSION['pseudo'])){?>
+                        <div class="mes_topics">
+                            <select class="mes_topics_menu" name="mes_topics" onChange="redirige(this.value)">
+                                <option value="" hidden>Mes topics</option>
+                                <option value="5">Mes articles</option>
+                            </select>
                         </div>
-                        <div class="contenu_haut">
-                            <div class="titre">
-                                <a class="titre_text" href="index.php?page=page_article&ID_auteur=<?php echo $result['ID_auteur'];?>&ID=<?php echo $result['ID']; ?>">
-                                    <p class="artitre"><?php  echo $result['Titre'];?></p>
-                                </a>
+                        <div class="crea_sujet">
+                            <a href="index.php?page=crea_article"><input class="button_crea_sujet" type="button" value="Créer un nouveau sujet"></a>
+                        </div> <?php
+                    } ?> 
+                </div>
+                <?php 
+                foreach ($resultes as $result) { ?>
+                    <div class="article">
+                        <div class="contenu">
+                            <div class="contenu_gauche">
+                                <div class="box_cat_img">
+                                    <img class="img_cat" src="<?php echo $result['images']; ?>">
+                                </div>
+                                <div class="contenu_haut">
+                                    <div class="titre">
+                                        <a class="titre_text" href="index.php?page=page_article&ID_auteur=<?php echo $result['ID_auteur'];?>&ID=<?php echo $result['ID']; ?>">
+                                            <p class="artitre"><?php  echo $result['Titre'];?></p>
+                                        </a>
+                                    </div>
+                                </div>
                             </div>
-                            
-                        </div>
-                    </div>
-                    <div class="contenu_droite">
-                        <div class="pdp_aut">
-                            <div class="pdp">
-                                <img class="imgpdp" src="<?php echo $result['Avatar_Path'];  ?>" alt=""/>
-                            </div>
-                            <div class="auteur">
-                                <?php echo $result['Pseudo']; ?>
-                            </div>
-                        </div>
-                        <div class="date">
-                          <?php  echo 'Le '.$result['Date_Publication']; ?>
-                        </div>
-                        <?php
+                            <div class="contenu_droite">
+                                <div class="pdp_aut">
+                                    <div class="pdp">
+                                        <img class="imgpdp" src="<?php echo $result['Avatar_Path'];  ?>" alt=""/>
+                                    </div>
+                                    <div class="auteur">
+                                        <?php echo $result['Pseudo']; ?>
+                                    </div>
+                                </div>
+                                <div class="date">
+                                  <?php  echo 'Le '.$result['Date_Publication']; ?>
+                                </div>
+                                <?php
                                 if (isset($_SESSION['pseudo'])){
                                     if($_SESSION['IsAdmin']==1){?>
                                     <li class="bouton_plus">
                                         <a class="button_plus" type="button">...</a>
                                         <ul class="box_supprimer">
-                                    
                                             <li><a class="white" href="index.php?page=delete&delete_post_id=<?php echo $result['ID'];?>">Supprimer</a></li>
-                                             <?php if($result['IsClosed']==0){ ?>
-                                                    <li>
-                                                        <div class="button_clore"><a class="white" href="index.php?page=opencloseart&id=<?php echo $result['ID'];?>&opcl=0">Clore</a><li>
+                                            <?php 
+                                            if($result['IsClosed']==0){ ?>
+                                                <li>
+                                                    <div class="button_clore"><a class="white" href="index.php?page=opencloseart&id=<?php echo $result['ID'];?>&opcl=0">Clore</a>
+                                                </li>
                                                 <?php   }
-                                                    else if($result['IsClosed']==1){ ?>
-                                                    <li><a class="white" href="index.php?page=opencloseart&id=<?php echo $result['ID'];?>&opcl=1">Réouvrir</a><li>
-                                            <?php    } ?>
+                                                else if($result['IsClosed']==1){ ?>
+                                                    <li>
+                                                        <a class="white" href="index.php?page=opencloseart&id=<?php echo $result['ID'];?>&opcl=1">Réouvrir</a>
+                                                    </li>
+                                        <?php   } ?>
                                         </ul>
                                     </li>
                               <?php }
                                     else if($_SESSION['ID']==$result['ID_auteur']){ ?>
                                         <ul>
-                                            <li><div class="button_plu"><input class="button_plus"type="button" /> </div>
+                                            <li>
+                                                <div class="button_plu">
+                                                    <input class="button_plus"type="button"/> 
+                                                </div>
                                                 <ul class="options">
-                                                    <li><div class="button_supprimer"><a href="index.php?page=delete&delete_post_id=<?php echo $result['ID'];?>">Supprimer</a></div></li>
+                                                    <li>
+                                                        <div class="button_supprimer"><a href="index.php?page=delete&delete_post_id=<?php echo $result['ID'];?>">Supprimer</a></div>
+                                                    </li>
                                                 </ul>
                                             </li>
                                         </ul>
                               <?php } 
                                 }?>
+                            </div>
+                        </div>
                     </div>
-                </div>
+          <?php }  ?>
             </div>
-      <?php }  ?>
-        </div>
         </div>
     </body>
 </html>
